8d33d00bc89b9f1b337f477f9d433b4adf7c1d5b
kc8 <satiristica@gmail.com>
2026-09-11 13:49:40 +0500

    feat(builder): made interface CharacterBuilderr

--- GameCharacterBuilder/src/main/java/gamecharacter/CharacterBuilder.java
+++ GameCharacterBuilder/src/main/java/gamecharacter/CharacterBuilder.java
@@ -1,5 +1,8 @@
 package gamecharacter;
 
-public class CharacterBuilder {
-    
+public interface CharacterBuilder {
+    CharacterBuilder setRace(String race);
+    CharacterBuilder setRole(String role);
+    CharacterBuilder setHealth(int health);
+    CharacterBuilder setWeapon(String weapon);
 }
