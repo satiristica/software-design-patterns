83efdbae7238799492d9640083d0a2b6d3ff7478
kc8 <satiristica@gmail.com>
2026-09-11 20:00:00 +0500

    feat(client): added client, demonstrating both character representations: object and JSON

--- GameCharacterBuilder/src/main/java/gamecharacter/Main.java
+++ GameCharacterBuilder/src/main/java/gamecharacter/Main.java
@@ -1,5 +1,22 @@
 package gamecharacter;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        CharacterDirector director = new CharacterDirector();
+        CharacterObjectBuilder objBuilder = new CharacterObjectBuilder();
+
+        director.makeKnight(objBuilder);
+
+        GameCharacter knight = objBuilder.getResult();
+
+
+        CharacterJsonBuilder jsonBuilder = new CharacterJsonBuilder();
+        
+        director.makeKnight(jsonBuilder);
+
+        String knightJson = jsonBuilder.getResult();
+
+        System.out.println(knight);
+        System.out.println(knightJson);
+    }
 }
