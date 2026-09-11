50e211280607d0749736c056c338cfc14d55c236
kc8 <satiristica@gmail.com>
2026-09-11 13:44:29 +0500

    feat(product): created immutable GameCharacter

--- GameCharacterBuilder/src/main/java/gamecharacter/GameCharacter.java
+++ GameCharacterBuilder/src/main/java/gamecharacter/GameCharacter.java
@@ -1,5 +1,22 @@
 package gamecharacter;
 
-public class GameCharacter {
+public final class GameCharacter {
 
+    private final String race;
+    private final String role;
+    private final int health;
+    private final String weapon;
+
+    GameCharacter(
+        String race,
+        String role,
+        int health,
+        String weapon
+    ) {
+        this.race = race;
+        this.role = role;
+        this.health = health;
+        this.weapon = weapon;
+    }
+    
 }
