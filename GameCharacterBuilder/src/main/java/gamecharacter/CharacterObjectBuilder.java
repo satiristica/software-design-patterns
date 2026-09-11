967d290d6b9ef5fd9e5f352bfeae3f2c262d6d36
kc8 <satiristica@gmail.com>
2026-09-11 18:27:45 +0500

    feat(builder): added validation and getResult(), fluent workflow of concrete builder

--- GameCharacterBuilder/src/main/java/gamecharacter/CharacterObjectBuilder.java
+++ GameCharacterBuilder/src/main/java/gamecharacter/CharacterObjectBuilder.java
@@ -33,4 +33,32 @@ public class CharacterObjectBuilder implements CharacterBuilder {
         this.weapon = weapon;
         return this;
     }
+
+
+    public GameCharacter getResult() {
+        validate();
+        return new GameCharacter(race, role, health, weapon);
+    }
+
+
+    public void validate() {
+        if (race == null || race.isBlank()) {
+            throw new IllegalStateException("Enter the race.");
+        }   
+
+
+        if (role == null || role.isBlank()) {
+            throw new IllegalStateException("Enter the role.");
+        }
+
+
+        if (health <= 0) {
+            throw new IllegalStateException("Health must be greater than 0.");
+        }
+
+
+        if (weapon == null || weapon.isBlank()) {
+            throw new IllegalStateException("Enter the weapon.");
+        }
+    }
 }
