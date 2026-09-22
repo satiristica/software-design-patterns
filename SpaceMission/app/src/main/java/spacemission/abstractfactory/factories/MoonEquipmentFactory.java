513eba26995538a93105b9c4080309efcdfbb5df
kc8 <satiristica@gmail.com>
2026-09-23 01:33:16 +0500

    added moon concrete factory

--- SpaceMission/app/src/main/java/spacemission/abstractfactory/factories/MoonEquipmentFactory.java
+++ SpaceMission/app/src/main/java/spacemission/abstractfactory/factories/MoonEquipmentFactory.java
@@ -1,5 +1,26 @@
 package spacemission.abstractfactory.factories;
 
-public class MoonEquipmentFactory {
-    
+import spacemission.abstractfactory.products.CommunicationSystem;
+import spacemission.abstractfactory.products.EnvironmentalSensor;
+import spacemission.abstractfactory.products.NavigationModule;
+import spacemission.abstractfactory.products.moon.MoonCommunicationSystem;
+import spacemission.abstractfactory.products.moon.MoonEnvironmentalSensor;
+import spacemission.abstractfactory.products.moon.MoonNavigationModule;
+
+public class MoonEquipmentFactory implements EquipmentFactory {
+
+    @Override
+    public EnvironmentalSensor createEnvironmentalSensor() {
+        return new MoonEnvironmentalSensor();
+    }
+
+    @Override
+    public CommunicationSystem createCommunicationSystem() {
+        return new MoonCommunicationSystem();
+    }
+
+    @Override
+    public NavigationModule createNavigationModule() {
+        return new MoonNavigationModule();
+    }
 }
