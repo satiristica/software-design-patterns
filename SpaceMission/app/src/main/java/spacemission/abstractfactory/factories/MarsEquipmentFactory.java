b01f34a447ef30180b6addf25b07cbec5595853e
kc8 <satiristica@gmail.com>
2026-09-23 01:30:54 +0500

    added concrete factory of mars

--- SpaceMission/app/src/main/java/spacemission/abstractfactory/factories/MarsEquipmentFactory.java
+++ SpaceMission/app/src/main/java/spacemission/abstractfactory/factories/MarsEquipmentFactory.java
@@ -1,5 +1,26 @@
 package spacemission.abstractfactory.factories;
 
-public class MarsEquipmentFactory {
-    
+import spacemission.abstractfactory.products.CommunicationSystem;
+import spacemission.abstractfactory.products.EnvironmentalSensor;
+import spacemission.abstractfactory.products.NavigationModule;
+import spacemission.abstractfactory.products.mars.MarsCommunicationSystem;
+import spacemission.abstractfactory.products.mars.MarsEnvironmentalSensor;
+import spacemission.abstractfactory.products.mars.MarsNavigationModule;
+
+public class MarsEquipmentFactory implements EquipmentFactory {
+
+    @Override
+    public EnvironmentalSensor createEnvironmentalSensor() {
+        return new MarsEnvironmentalSensor();
+    }
+
+    @Override
+    public CommunicationSystem createCommunicationSystem() {
+        return new MarsCommunicationSystem();
+    }
+
+    @Override
+    public NavigationModule createNavigationModule() {
+        return new MarsNavigationModule();
+    }
 }
