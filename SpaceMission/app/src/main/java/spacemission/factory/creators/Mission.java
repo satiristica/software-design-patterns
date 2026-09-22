e039328055e883e1793e9accd7a19dd03a25f600
kc8 <satiristica@gmail.com>
2026-09-22 22:51:59 +0500

    feat(creator): add mission creator

--- SpaceMission/app/src/main/java/spacemission/factory/creators/Mission.java
+++ SpaceMission/app/src/main/java/spacemission/factory/creators/Mission.java
@@ -1,6 +1,21 @@
 package spacemission.factory.creators;
 
+import spacemission.factory.products.Spacecraft;
 
-public class Mission {
-    
+public abstract class Mission {
+
+    protected abstract Spacecraft createSpacecraft();
+
+    public final void launchMission() {
+        Spacecraft spacecraft = createSpacecraft();
+
+        System.out.println("Preparing mission");
+        spacecraft.deploy();
+
+        if (!spacecraft.isOperational()) {
+            throw new IllegalStateException("Deploy failed");
+        }
+
+        System.out.println("Mission is operational");
+    }
 }
