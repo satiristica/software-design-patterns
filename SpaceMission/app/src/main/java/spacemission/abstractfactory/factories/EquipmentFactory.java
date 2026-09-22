253df1503ee6bfa1e4dbb1dbbcd4d3ce38fdf108
kc8 <satiristica@gmail.com>
2026-09-23 01:29:11 +0500

    added equipment factory interface

--- SpaceMission/app/src/main/java/spacemission/abstractfactory/factories/EquipmentFactory.java
+++ SpaceMission/app/src/main/java/spacemission/abstractfactory/factories/EquipmentFactory.java
@@ -1,5 +1,14 @@
 package spacemission.abstractfactory.factories;
 
-public class EquipmentFactory {
-    
+import spacemission.abstractfactory.products.CommunicationSystem;
+import spacemission.abstractfactory.products.EnvironmentalSensor;
+import spacemission.abstractfactory.products.NavigationModule;
+
+public interface EquipmentFactory {
+
+    EnvironmentalSensor createEnvironmentalSensor();
+
+    CommunicationSystem createCommunicationSystem();
+
+    NavigationModule createNavigationModule();
 }
