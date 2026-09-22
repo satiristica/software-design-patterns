53f4f6debb5ab3cb7a98720ea75ee03cc14666ea
kc8 <satiristica@gmail.com>
2026-09-22 22:53:52 +0500

    feat(creator): add concrete creator rover

--- SpaceMission/app/src/main/java/spacemission/factory/creators/RoverMission.java
+++ SpaceMission/app/src/main/java/spacemission/factory/creators/RoverMission.java
@@ -1,6 +1,11 @@
 package spacemission.factory.creators;
 
+import spacemission.factory.products.Rover;
+import spacemission.factory.products.Spacecraft;
 
-public class RoverMission {
-    
+public class RoverMission extends Mission {
+    @Override
+    protected Spacecraft createSpacecraft() {
+        return new Rover();
+    }
 }
